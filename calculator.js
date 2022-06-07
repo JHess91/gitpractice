8fd78a22e60f0746e3bf317db8aae6a0f9f89918
Justin <justin@Justins-MacBook-Air.local>
2022-06-07 19:31:52 -0400

    Calc update

--- calculator.js
+++ calculator.js
@@ -5,52 +5,52 @@ console.log('')
 console.log('Please read the acceptance criteria to get started.')
 console.log('')
 
-
-const name = prompt('What is your name? ')
+const name = prompt('Hello human! What is your name? ')
 console.log(`Hey there ${name}`)
 
-function calculator(){
+let tryAgain = true
 
-const operation = prompt(`Please choose an operation: + , - , * , / `)
+while (tryAgain == true){
+   
+  const operation = prompt(`Please choose an operation: + , - , * , / `)
 
-let first = prompt(`Enter your first number `)
-let second = prompt(`Enter your second number `)
+  let first = prompt(`Enter your first number `)
+  let second = prompt(`Enter your second number `)
 
-const x = Number(first)
-const y = Number(second)
+  const x = Number(first)
+  const y = Number(second)
 
-switch (operation) {
-  case '+':
-    console.log(x + y)
-    break
-  case '-':
-    console.log(x - y)
-    break
-  case '*':
-    console.log(x * y)
-    break
-  case '/':
-    console.log(x / y)
-    break
-}
-}
-
-calculator(console.log())
-
-const tryAgain = prompt('Would you like to run Calculator again? (Yes/No) ')
-
-const yes = 'Yes'
-const no = 'No'
-
-while (tryAgain){
-switch(tryAgain){
-  case 'Yes':
-    calculator(console.log())
-    break
-    case 'No':
-      console.log(`Thank you for using Justin's calculator`)
+  switch (operation) {
+    case '+':
+      console.log(x + y)
       break
+    case '-':
+      console.log(x - y)
+      break
+    case '*':
+      console.log(x * y)
+      break
+    case '/':
+      console.log(x / y)
+      break
+  }
+  
 
-      
+   reRun = prompt('Would you like to run Calculator again? (Yes/No) ')
+   
+  if (reRun == 'Yes'){
+    // tryAgain == true
+    console.clear
+ } if (reRun == 'No'){  // tryAgain == false
+    console.clear()
+    console.log(`Have a great day!`)
+    process.exit()
 }
+
 }
+
+
+
+
+
+
