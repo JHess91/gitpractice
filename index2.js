236b46087f9205c6f773d00b0f74a2e610a6715c
Justin <justin@Justins-MacBook-Air.local>
2022-06-10 17:34:48 -0400

    NewCalc

--- index2.js
+++ index2.js
@@ -15,38 +15,44 @@ console.log("")
 let runAgain = true
 
 while(runAgain == true) {
-    console.log(`Now how would you like to math?`)
-    const operation = prompt(`Add, Subtract, Multiply, Divide`)
+    console.log(`Now how would you like to math? `)
+    console.log("")
+    const operation = prompt(`Add, Subtract, Multiply, Divide `)
+    console.log("")
  
     console.log(`Great choice. Now lets pick two numbers to ${operation}`)
+    console.log("")
 
     let firstNum = prompt(`Enter your first number `)
+    console.log("")
     let secondNum = prompt(`Enter your second number `)
+    console.log("")
 
     const x = Number(firstNum)
     const y = Number(secondNum)
 
     switch (operation) {
-        case 'Add':
+        case 'Add' || '+' || 'add':
           console.log(x + y)
           break
-        case 'Subtract':
+        case 'Subtract' || '-' || 'subtract':
           console.log(x - y)
           break
-        case 'Multiply':
+        case 'Multiply' || '*' || 'multiply':
           console.log(x * y)
           break
-        case 'Divide':
+        case 'Divide' || '/' || 'divide':
           console.log(x / y)
           break
       }
 
       console.log(`Wanna try another one?`)
+      console.log("")
       startOver = prompt (`Yes / No `)
 
-      if (startOver == 'Yes'){
+      if (startOver === 'Yes'){
           console.clear
-      } else if (startOver == 'No') {
+      } else if (startOver === 'No') {
           console.clear()
           console.log(`Well that was fun! Let me know if you need me again. Dont forget about me.`)
           process.exit()
